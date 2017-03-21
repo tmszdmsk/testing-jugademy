2bc1948535d4ba8a4a70334e149cc2aae88fe5d3
Tomasz Adamski <tomasz.adamski@gmail.com>
2017-03-21 23:33:47 +0100

    spock demo prep

--- 3-spock/src/test/groovy/FizzBuzzSpec.groovy
+++ 3-spock/src/test/groovy/FizzBuzzSpec.groovy
@@ -8,30 +8,6 @@ class FizzBuzzSpec extends Specification {
 
     def fizzBuzz = new FizzBuzz()
 
-    def "should return #expected for #number"(number, expected) {
-        expect:
-        fizzBuzz.calculate(number) == expected
-
-        where:
-        number | expected
-        1      | "12"
-        3      | "Fizz"
-        5      | "Buzz"
-        15     | "FizzBuzz"
-    }
-
-    def "should throw exception for #number"(number) {
-        when:
-        fizzBuzz.calculate(number)
-
-        then:
-        thrown(IllegalArgumentException)
-
-        where:
-        number << [-1, 0, 101]
-    }
-
-
     def "result from collector should contain 3"() {
         setup:
         def collector = new FizzBuzzCollector(fizzBuzz)
@@ -42,4 +18,59 @@ class FizzBuzzSpec extends Specification {
         then:
         result.contains("3")
     }
+
+
+    def "should throw exception for #number"(number) {
+        setup:
+
+        given:
+
+        when:
+        fizzBuzz.calculate(number)
+
+        then:
+        thrown(IllegalArgumentException)
+
+        where:
+        number << [-1, 0, 101]
+    }
 }
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//def "should return #expected for #number"(number, expected) {
+//    expect:
+//    fizzBuzz.calculate(number) == expected
+//
+//    where:
+//    number | expected
+//    1      | "12"
+//    3      | "Fizz"
+//    5      | "Buzz"
+//    15     | "FizzBuzz"
+//}
