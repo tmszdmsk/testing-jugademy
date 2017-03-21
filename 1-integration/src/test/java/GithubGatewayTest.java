6c49be84cb39860f9554d10722fd5526b4825ed4
Tomasz Adamski <tomasz.adamski@gmail.com>
2017-03-21 23:22:05 +0100

    integration test demo prep

--- 1-integration/src/test/java/GithubGatewayTest.java
+++ 1-integration/src/test/java/GithubGatewayTest.java
@@ -9,40 +9,31 @@ import java.io.IOException;
 
 public class GithubGatewayTest {
 
-  GithubGateway githubGateway = new GithubGateway();
+    GithubGateway githubGateway;
 
-  @Rule
-  public WireMockRule wireMock = new WireMockRule(8080);
+    @Before
+    public void setUp() {
+        githubGateway = new GithubGateway();
+    }
 
-  @Before
-  public void setUp() {
-    githubGateway = new GithubGateway("http://localhost:8080");
-  }
+    @Test
+    public void shouldReturnPoznanForTmszdmsk() throws IOException {
+        //given
 
-  @Test
-  public void shouldReturnPoznanForTmszdmsk() throws IOException {
-    //given
-    wireMock.stubFor(WireMock.get(WireMock.urlPathEqualTo("/users/tmszdmsk"))
-                         .willReturn(
-                             WireMock.aResponse().withBody("{\"location\": \"Poznań, Poland\"}")));
+        //when
+        String location = githubGateway.getUserLocation("tmszdmsk");
 
-    //when
-    String location = githubGateway.getUserLocation("tmszdmsk");
+        //then
+        Assertions.assertThat(location).contains("Poznań");
+    }
 
-    //then
-    Assertions.assertThat(location).contains("Poznań");
-  }
+    @Test(expected = GithubUnavailableException.class)
+    public void shouldThrowGithubUnavailableWhenGithubFails() throws IOException {
+        //given
 
-  @Test(expected = GithubUnavailableException.class)
-  public void shouldThrowGithubUnavailableWhenGithubFails() throws IOException {
-    //given
-    wireMock.stubFor(
-        WireMock
-            .get(WireMock.urlPathEqualTo("/users/tmszdmsk"))
-            .willReturn(WireMock.aResponse().withStatus(500)));
-    //when
-    githubGateway.getUserLocation("tmszdmsk");
-  }
+        //when
+        githubGateway.getUserLocation("tmszdmsk");
+    }
 
 
 //  @Test
@@ -64,3 +55,95 @@ public class GithubGatewayTest {
 //  }
 
 }
+
+
+
+
+
+
+
+
+
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//  GithubGateway githubGateway = new GithubGateway();
+//
+//  @Rule
+//  public WireMockRule wireMock = new WireMockRule(8080);
+//
+//  @Before
+//  public void setUp() {
+//    githubGateway = new GithubGateway("http://localhost:8080");
+//  }
+//
+//  @Test
+//  public void shouldReturnPoznanForTmszdmsk() throws IOException {
+//    //given
+//    wireMock.stubFor(WireMock.get(WireMock.urlPathEqualTo("/users/tmszdmsk"))
+//            .willReturn(
+//                    WireMock.aResponse().withBody("{\"location\": \"Poznań, Poland\"}")));
+//
+//    //when
+//    String location = githubGateway.getUserLocation("tmszdmsk");
+//
+//    //then
+//    Assertions.assertThat(location).contains("Poznań");
+//  }
+//
+//  @Test(expected = GithubUnavailableException.class)
+//  public void shouldThrowGithubUnavailableWhenGithubFails() throws IOException {
+//    //given
+//    wireMock.stubFor(
+//            WireMock
+//                    .get(WireMock.urlPathEqualTo("/users/tmszdmsk"))
+//                    .willReturn(WireMock.aResponse().withStatus(500)));
+//    //when
+//    githubGateway.getUserLocation("tmszdmsk");
+//  }
